40d1b6236a53b3a4cf12c00161a1da092d92c18e
shupeng <john.hi.gm@gmail.com>
2020-01-14 09:52:02 +0800

    beata

--- NutPool/NutPool.h
+++ NutPool/NutPool.h
@@ -8,6 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NutPool : NSObject
+/// 商品
+@protocol NutProduct <NSObject>
+@end
 
+/// 消费者
+@protocol NutConsumer <NSObject>
+@end
+
+/// 生产者
+typedef void (^NutProductCallback)(id <NutProduct> product, NSError *error);
+
+@protocol NutProducer <NSObject>
+- (void)produceProduct:(NutProductCallback)callback;
+@end
+
+@interface NutTask : NSObject
+@property(nonatomic, strong) NSString *name;
+@end
+
+
+@interface NutPool : NSObject
+@property(nonatomic, strong) id <NutProducer> producer;
+@property(nonatomic, assign) NSInteger poolCount;
+@property(nonatomic, assign) NSInteger maxRetryCount; // max retry times. default 3
+
+- (NutPool *)initWithProducer:(id <NutProducer>)producer poolCount:(NSInteger)poolCount;
+
+- (NutTask *)productTask:(NutProductCallback)callback;
+
+- (void)cancelProductTask:(NutTask *)task;
 @end
--- NutPool/NutPool.m
+++ NutPool/NutPool.m
@@ -8,6 +8,143 @@
 
 #import "NutPool.h"
 
+@implementation NutTask
+
+@end
+
+
+@interface NutPool ()
+// 生产者
+@property(nonatomic, strong) dispatch_queue_t produceQueue;// 生产任务执行队列（线程,worker）
+@property(nonatomic, strong) dispatch_semaphore_t produceSemaphore; // 可生产信号
+
+// 消费者
+@property(nonatomic, strong) NSOperationQueue *consumeQueue; // 消费任务执行队列（线程,worker）
+@property(nonatomic, strong) NSMutableArray *pendingList;// 正在等待的消费者列表
+@property(nonatomic, strong) dispatch_semaphore_t consumeSemaphore;// 可消费信号
+
+// 商品
+@property(nonatomic, strong) NSMutableArray *productList; // 商品列表
+@property(nonatomic, strong) NSLock *productLock; // 商品列表操作锁
+
+@property(nonatomic, assign) NSInteger currentRetryCount;
+@end
+
+void syncMain(void (^block)(void)) {
+    if ([NSThread isMainThread]) {
+        block();
+    } else {
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            block();
+        });
+    }
+}
+
 @implementation NutPool
 
+- (NutPool *)initWithProducer:(id <NutProducer>)producer poolCount:(NSInteger)poolCount {
+    static NSInteger counter = 0;
+
+    self = [super init];
+    if (self) {
+        self.maxRetryCount = 3;
+
+        self.producer = producer;
+        self.poolCount = poolCount;
+
+        self.produceQueue = dispatch_queue_create([[NSString stringWithFormat:@"nut_pool_produce_queue %@", @(counter)] cStringUsingEncoding:NSASCIIStringEncoding], DISPATCH_QUEUE_CONCURRENT);
+        self.produceSemaphore = dispatch_semaphore_create(0);
+
+        self.consumeQueue = [[NSOperationQueue alloc] init];
+        self.consumeQueue.maxConcurrentOperationCount = 1;
+        self.pendingList = [NSMutableArray array];
+        self.consumeSemaphore = dispatch_semaphore_create(poolCount);
+
+        self.productList = [NSMutableArray array];
+        self.productLock = [[NSLock alloc] init];
+
+        [self start];
+    }
+
+    return self;
+}
+
+- (NutTask *)productTask:(NutProductCallback)callback {
+    NutTask *task = [[NutTask alloc] init];
+    task.name = [[NSUUID UUID] UUIDString];
+
+    syncMain(^{
+        NSBlockOperation *operation = [self productTaskOperation:callback];
+        [self.pendingList addObject:@{task.name: operation}];
+        [self.consumeQueue addOperation:operation];
+    });
+
+    return task;
+}
+
+- (NSBlockOperation *)productTaskOperation:(NutProductCallback)callback {
+    NSBlockOperation *operation = [[NSBlockOperation alloc] init];
+    [operation addExecutionBlock:^{
+        dispatch_semaphore_wait(self.consumeSemaphore, DISPATCH_TIME_FOREVER);
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if ([operation isCancelled] && operation.isFinished == false) {
+                dispatch_semaphore_signal(self.consumeSemaphore);
+            } else {
+                [self.productLock lock];
+                id <NutProduct> product = self.productList.firstObject;
+                [self.productList removeObjectAtIndex:0];
+                [self.productLock unlock];
+
+                callback(product, nil);
+                dispatch_semaphore_signal(self.produceSemaphore);
+            }
+        });
+    }];
+
+    return operation;
+}
+
+- (void)cancelProductTask:(NutTask *)task {
+    // 任务的状态：正在被添加->已添加(等待商品)->正在派发商品->商品派发完毕完毕
+    syncMain(^{
+        __block NSDictionary * findObj = nil;
+        [self.pendingList enumerateObjectsUsingBlock:^(NSDictionary *obj, NSUInteger idx, BOOL *stop) {
+            if ([obj.allKeys.firstObject isEqualToString:task.name]) {
+                findObj = obj;
+                *stop = true;
+            }
+        }];
+
+        NSBlockOperation *operation = findObj.allValues.firstObject;
+        [operation cancel];
+        [self.pendingList removeObject:findObj];
+    });
+}
+
+- (void)start {
+    dispatch_async(self.produceQueue, ^{
+        while (true) {
+            dispatch_semaphore_wait(self.produceSemaphore, DISPATCH_TIME_FOREVER);
+
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+                [self.producer produceProduct:^(id <NutProduct> product, NSError *error) {
+                    if (error) {
+                        NSLog(@"[NutPool] produce product failed: %@", [error localizedDescription]);
+
+                        self.currentRetryCount++;
+                        if (self.currentRetryCount < self.maxRetryCount) {
+                            dispatch_semaphore_signal(self.produceSemaphore);
+                        }
+                    } else {
+                        [self.productLock lock];
+                        [self.productList addObject:product];
+                        dispatch_semaphore_signal(self.consumeSemaphore);
+                        [self.productLock unlock];
+                    }
+                }];
+            });
+        }
+    });
+}
 @end
